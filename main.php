4193b74ce3f25e746a503354a0cce63a6a0eee48
Hirohisa Kawase <hirokws@gmail.com>
2015-02-04 05:58:52 +0900

    glob対象を詳細に指定

--- main.php
+++ main.php
@@ -39,9 +39,17 @@ if( array_key_exists( 't', $options ) || array_key_exists( 'tab', $options ) ||
     print 'タブ変換開始…'.PHP_EOL;
 
     $file = new File();
-    $files = $file->globAll(__DIR__.'/../../..', '*');
+    $files = $file->globAll( __DIR__.'/../../../app', '*' );
+    $files += $file->globAll( __DIR__.'/../../../bootstrap', '*' );
+    $files += $file->globAll( __DIR__.'/../../../config', '*' );
+    $files += $file->globAll( __DIR__.'/../../../database', '*' );
+    $files += $file->globAll( __DIR__.'/../../../resources/lang', '*' );
+    $files += $file->globAll( __DIR__.'/../../../resources/views', '*' );
+    $files += $file->globAll( __DIR__.'/../../../tests', '*' );
+    $files += [ __DIR__.'/../../../artisan'];
+    $files += [ __DIR__.'/../../../server.php'];
 
-    var_dump($files); die();
+    var_dump( $files ); die();
 
     print 'タブ変換終了'.PHP_EOL;
 }
