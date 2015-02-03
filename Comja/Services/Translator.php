88f306c98570cb2c6e587e46129a90f000d360ae
Hirohisa Kawase <hirokws@gmail.com>
2015-02-03 10:06:46 +0900

    翻訳追加、ファイル読み書き時のエラー処理追加

--- Comja/Services/Translator.php
+++ Comja/Services/Translator.php
@@ -12,16 +12,26 @@ class Translator
 
     public function trans( $translations )
     {
-        foreach ($translations as $fileName => $transArray)
+        foreach( $translations as $fileName => $transArray )
         {
             // ファイル読み込み
-            $contents = file_get_contents($fileName);
+            $contents = file_get_contents( $fileName );
+            if( !$contents )
+            {
+                fputs( STDERR, 'ファイル:'.$fileName.'を読み込めませんでした。'.PHP_EOL );
+                continue;
+            }
 
             // 英語 => 日本語変換
-            $translatedContent = str_replace(array_keys($transArray), array_values($transArray), $contents);
+            $translatedContent = str_replace( array_keys( $transArray ), array_values( $transArray ), $contents );
 
             // ファイル書き出し
-            file_put_contents($fileName, $translatedContent);
+            $ret = file_put_contents( $fileName, $translatedContent );
+            if( !$ret )
+            {
+                fputs( STDERR, 'ファイル:'.$fileName.'へ書き込めませんでした。'.PHP_EOL );
+                continue;
+            }
         }
     }
 
