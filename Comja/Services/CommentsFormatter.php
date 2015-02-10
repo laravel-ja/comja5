30912672eca3782f35576e2c93828004468c4511
Hirohisa Kawase <hirokws@gmail.com>
2015-02-10 18:35:07 +0900

    コメント削除追加

---
+++ Comja/Services/CommentsFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Comja\Services;
+
+class CommentsFormatter
+{
+
+    public function remove( $filePath )
+    {
+        $content = file_get_contents( $filePath );
+        if( $content === false )
+        {
+            fputs( STDERR, 'ファイル名:'.$fileName.'が読み込めません。'.PHP_EOL );
+            return;
+        }
+
+        // 行コメント削除
+        $noLineComment = preg_replace( '#^\s*//.*$#m', "", $content );
+
+        // ブロックコメント削除
+        $noBlockComment = preg_replace( '#/\*.+\*/#sU', '', $noLineComment );
+
+        // 空行削除
+        $noSpaceLine = preg_replace( '/^\s*\n/m', '', $noBlockComment );
+
+        // 最終行に空行追加
+        $addedEmptyLine = rtrim( $noSpaceLine, "\n" )."\n";
+
+        $ret = file_put_contents( $filePath, $addedEmptyLine );
+//        if( $ret === false )
+//        {
+//            fputs( STDERR, 'ファイル名:'.$fileName.'が書き込めません。'.PHP_EOL );
+//            return;
+//        }
+    }
+
+}
--- main.php
+++ main.php
@@ -6,20 +6,43 @@ use Comja\Repositories\TranslationRepo;
 use Comja\Services\Translator;
 use Comja\Services\File;
 use Comja\Services\TabFormatter;
+use Comja\Services\CommentsFormatter;
 
 // オプションの取り込み
 
-$options = getopt( "ctfar", ["comment", "tab", "file", "all" ] );
+$options = getopt( "ctfar", ["comment", "tab", "file", "remove", "all" ] );
 
 if( $options === false || count( $options ) < 1 )
 {
     fputs( STDERR, "オプション指定がありません。".PHP_EOL );
-    print "使用法： comja [-c|--comment] [-t|--tab] [-f|--file] [-a|--all]".PHP_EOL;
+    print "使用法： comja [-c|--comment] [-t|--tab] [-f|--file] [-r|--remove] [-a|--all] [-A]".PHP_EOL;
     print "オプション：".PHP_EOL;
     print "-c --comment：コメント部分の翻訳".PHP_EOL;
     print "-t --tab：タブを4スペースへ変換".PHP_EOL;
     print "-f --file：日本語言語ファイル生成".PHP_EOL;
-    print "-a --all：上記の3アクションを行います".PHP_EOL;
+    print "-r --remove：コメント／空行削除".PHP_EOL;
+    print "-a --all：翻訳、タブ変換、言語ファイル追加を行います".PHP_EOL;
+    print "-A：コメント削除、タブ変換、言語ファイル追加を行います".PHP_EOL;
+}
+
+if( (array_key_exists( 'c', $options ) || array_key_exists( 'comment', $options )) &&
+    (array_key_exists( 'r', $options ) || array_key_exists( 'remove', $options )) )
+{
+    fputs( STDERR, 'コメント翻訳とコメント削除は、同時に指定できません。'.PHP_EOL );
+    return 1;
+}
+
+if( (array_key_exists( 'a', $array ) || array_key_exists( 'all', $array )) &&
+    count( $options ) != 1 )
+{
+    fputs( STDERR, '-aまたは--allオプションは他のオプションと同時に指定できません。'.PHP_EOL );
+    return 1;
+}
+
+if( array_key_exists( 'A', $array ) && count( $options ) != 1 )
+{
+    fputs( STDERR, '-Aオプションは他のオプションと同時に指定できません。'.PHP_EOL );
+    return 1;
 }
 
 if( array_key_exists( 'c', $options ) || array_key_exists( 'comment', $options ) ||
@@ -86,4 +109,26 @@ if( array_key_exists( 'f', $options ) || array_key_exists( 'file', $options ) ||
 
     print '言語ファイル生成終了'.PHP_EOL;
 }
+
+if( array_key_exists( 't', $options ) || array_key_exists( 'tab', $options ) ||
+    array_key_exists( 'A', $options ) || array_key_exists( 'all', $options ) )
+{
+    print 'コメント削除開始…'.PHP_EOL;
+
+    $file = new File();
+    $files = array_merge(
+        $file->globAll( __DIR__.'/../../../app', '*' ), $file->globAll( __DIR__.'/../../../bootstrap', '*' ), $file->globAll( __DIR__.'/../../../config', '*' ), $file->globAll( __DIR__.'/../../../database', '*' ), $file->globAll( __DIR__.'/../../../resources/lang', '*' ), $file->globAll( __DIR__.'/../../../resources/views', '*' ), $file->globAll( __DIR__.'/../../../tests', '*' ) ); // NetBeansの整形が…
+    $files[] = __DIR__.'/../../../artisan';
+    $files[] = __DIR__.'/../../../server.php';
+
+    $commentsFormatter = new CommentsFormatter();
+
+    foreach( $files as $targetFile )
+    {
+        $commentsFormatter->remove( $targetFile );
+    }
+
+    print 'コメント削除終了'.PHP_EOL;
+}
+
 return 0;
