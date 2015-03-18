80b242069d569b9b0e2ee917733bf8daa89201a7
Hirohisa Kawase <hirokws@gmail.com>
2015-03-19 08:42:06 +0900

    helper関数に存在のチェック挿入

--- helper.php
+++ helper.php
@@ -1,11 +1,15 @@
 <?php
 
-function __( $message )
+if( !function_exists( '__' ) )
 {
-    if( strtoupper( substr( PHP_OS, 0, 3 ) ) === 'WIN' )
-    {
-        return mb_convert_encoding( $message, 'SJIS', 'UTF-8' );
-    }
 
-    return $message;
+    function __( $message )
+    {
+        if( strtoupper( substr( PHP_OS, 0, 3 ) ) === 'WIN' )
+        {
+            return mb_convert_encoding( $message, 'SJIS', 'UTF-8' );
+        }
+
+        return $message;
+    }
 }
