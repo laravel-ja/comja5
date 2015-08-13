ebaacede822fdfa5ba051ca9f0a6f1d1f300d61d
Hirohisa Kawase <hirokws@gmail.com>
2015-08-13 14:20:39 +0900

    Revert "Git shellの場合はUTF8のまま表示する"

--- helper.php
+++ helper.php
@@ -3,8 +3,7 @@
 if (!function_exists('c5_trans')) {
     function c5_trans($message)
     {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' &&
-            getenv('BASH') !== '/bin/sh') { // git shell対応
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             return mb_convert_encoding($message, 'SJIS', 'UTF-8');
         }
 
